8dbc4635ba57930e9a42326f9ca430fdcd14a235
Saki <ksaki0402@gmail.com>
2025-07-21 15:58:56 +0900

    Initial Commit

---
+++ WordList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordList
+//
+//  Created by 神林沙希 on 2025/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordList/WordListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordListApp.swift
+//  WordList
+//
+//  Created by 神林沙希 on 2025/07/21.
+//
+
+import SwiftUI
+
+@main
+struct WordListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
